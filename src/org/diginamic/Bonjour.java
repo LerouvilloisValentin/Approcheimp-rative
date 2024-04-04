1ac92c984a4ff1cbb7d7549d180e3ab7f91c9b6f
LerouvilloisValentin <lerouvillois.valentin61@hotmail.fr>
2024-04-04 14:00:24 +0200

    first commit

---
+++ src/org/diginamic/Bonjour.java
@@ -0,0 +1,10 @@
+package org.diginamic;
+
+public class Bonjour {
+
+	public static void main(String[] args) {		// TODO Auto-generated method stub
+		System.out.println("Bonjour tout le monde");
+		
+	}
+
+}
